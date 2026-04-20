ded283ed77b12817377740d00070845b7815da16
LaurenAricaG <laurendavid159@gmail.com>
2026-04-20 18:44:44 -0500

    eat: Add JWT session configuration in NextAuth

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -5,6 +5,9 @@ import { prisma } from "@/lib/prisma";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   secret: process.env.AUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
 
   cookies: {
     sessionToken: {
